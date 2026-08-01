jannau/m1n1

commit b3bd2bd906477fa68659e63c5aec36f642605424

isp: Add t6030 initialization
Signed-off-by: Sasha Finkelstein <k@chaosmail.tech>

src/isp.c
@@ -14,12 +14,13 @@
 #define ISP_VER_T6000 0xb3091
 #define ISP_VER_T8112 0xc1090
 #define ISP_VER_T6020 0xc3091
-#define ISP_VER_T6031 0xf3001
+#define ISP_VER_T603X 0xf3001
 
 // PMGR offset to enable to get the version info to work
 #define ISP_PMGR_T8103 0x4018
 #define ISP_PMGR_T6000 0x8
 #define ISP_PMGR_T6020 0x4008
+#define ISP_PMGR_T6030 0x3c8
 #define ISP_PMGR_T6031 0x4030
 
 static bool isp_initialized = false;
@@ -90,6 +91,9 @@ int isp_init(void)
         case T6020 ... T6022:
             pmgr_off = ISP_PMGR_T6020;
             break;
+        case T6030:
+            pmgr_off = ISP_PMGR_T6030;
+            break;
         case T6031 ... T6034:
             pmgr_off = ISP_PMGR_T6031;
             break;
@@ -150,7 +154,7 @@ int isp_init(void)
                     return -1;
             }
             break;
-        case ISP_VER_T6031:
+        case ISP_VER_T603X:
             switch (os_firmware.version) {
                 case V14_7:
                     heap_top = 0x1000000;

commit 507aed3f71a74e83faf338b93f9c2bd866f5df3d

isp: Fix t6030 PMGR offset
T6030' ISP_FE power-state has the same offset as T6020 so use the
existing define / case.

Fixes: b3bd2bd9 ("isp: Add t6030 initialization")
Signed-off-by: Janne Grunau <j@jannau.net>

src/isp.c
@@ -20,7 +20,6 @@
 #define ISP_PMGR_T8103 0x4018
 #define ISP_PMGR_T6000 0x8
 #define ISP_PMGR_T6020 0x4008
-#define ISP_PMGR_T6030 0x3c8
 #define ISP_PMGR_T6031 0x4030
 
 static bool isp_initialized = false;
@@ -89,10 +88,8 @@ int isp_init(void)
             pmgr_off = ISP_PMGR_T6000;
             break;
         case T6020 ... T6022:
-            pmgr_off = ISP_PMGR_T6020;
-            break;
         case T6030:
-            pmgr_off = ISP_PMGR_T6030;
+            pmgr_off = ISP_PMGR_T6020;
             break;
         case T6031 ... T6034:
             pmgr_off = ISP_PMGR_T6031;