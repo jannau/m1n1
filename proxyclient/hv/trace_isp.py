f23e11e187706a2a9a6c46f398852ce478e42334
Janne Grunau <j@jannau.net>
2026-08-03 22:46:17 +0200

    hv/trace_isp: Turn ISP_SYS power-state one for dart-isp
    T8122 and T6030 ADTs miss a power-gates property in dart-isp. Turn
    ISP_SYS manually on so that the tracer can use dart-isp.
    
    Signed-off-by: Janne Grunau <j@jannau.net>

--- proxyclient/hv/trace_isp.py
+++ proxyclient/hv/trace_isp.py
@@ -8,6 +8,13 @@ dev_name = [node.name for node in u.adt["/arm-io"] if node.name.startswith("isp"
 dev_path = "/arm-io/%s" % (dev_name)
 dart_dev_path = "/arm-io/dart-%s" % (dev_name)
 print(dev_path, dart_dev_path)
+
+# t8122 and t6030 have no power-gates in dart-isp, turn ISP_SYS on manually
+if u.adt["/chosen"].chip_id == 0x8122:
+    hv.p.pmgr_power_enable(45)
+elif u.adt["/chosen"].chip_id == 0x6030:
+    hv.p.pmgr_power_enable(104)
+
 tracer = ISPTracer(hv, dev_path, dart_dev_path, verbose=4)
 tracer.start()
 dart = tracer.dart_tracer.dart
